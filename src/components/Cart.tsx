10287dfa0421136a36fc8647bb967f26b2f13f48
Roshni Mandal <rmandal@griddynamics.com>
2025-05-28 19:34:42 +0530

    chnaged to cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -4,6 +4,17 @@ const Cart : React.FC = () => {
   return (
     <div>
       this is cart section.
+      <div className="cart-item">
+        <div className="cart-item-image">
+            <img src="/Bitmap.svg" alt="cart-item-image" />
+        </div>
+        <div className="cart-item-details">
+            <h3>Product Name</h3>
+            <p>Product Description</p>
+            <p>Product Price</p>
+        </div>
+        <div className="cart-item-quantity"></div>
+      </div>
     </div>
   )
 }
