ef70f362c408352287bc5e25851585e4ed30f1b2
Roshni Mandal <rmandal@griddynamics.com>
2025-05-28 20:32:38 +0530

    cart3 added

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ const App: React.FC = () => {
         <Route path="/cart" element={< Cart/>} />
         
         <Route path="/cart1" element={< Cart/>} />
+        <Route path="/cart3" element={< Cart/>} />
       </Routes>
     </Router>
   )
